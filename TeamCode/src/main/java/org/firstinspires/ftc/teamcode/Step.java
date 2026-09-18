cdd5dbec0c79245313f478f877bc9a531a5e94c9
aaclay1 <74253206+aaclay1@users.noreply.github.com>
2026-09-18 10:25:27 -0500

    reset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Step.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-
-public interface Step {
-    enum Status { RUNNING, SUCCESS, FAILURE }
-    Status runStep(TelemetryPacket packet);
-}
